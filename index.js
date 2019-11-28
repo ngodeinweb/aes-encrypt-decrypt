25dbb3c10678898df261580553cc28b306ae8f01
ngodeinweb <38836053+ngodeinweb@users.noreply.github.com>
2019-11-28 11:34:56 +0700

    Enjoy to use

---
+++ index.js
@@ -0,0 +1,15 @@
+// simple project by Yudi Arif Kurniawan
+// Developer URI : https://ngodeinweb.com/
+
+function encrypt(){
+  var inputan = document.getElementById('inputan').value;
+  var encrypted = CryptoJS.AES.encrypt(inputan, "Ngodeinweb");
+  document.getElementById('app').innerHTML = encrypted;
+}
+
+function decrypte(){
+  var inputan2 = document.getElementById('inputan2').value;
+  var decrypted = CryptoJS.AES.decrypt(inputan2, "Ngodeinweb");
+  document.getElementById('app2').innerHTML = decrypted.toString(CryptoJS.enc.Utf8);
+}
+
